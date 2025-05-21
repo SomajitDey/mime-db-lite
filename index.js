19491c516abe3016c404e0c0a9fb7fff8c7c4ddc
Somajit Dey <73181168+SomajitDey@users.noreply.github.com>
2025-05-21 17:11:14 +0530

    1.0.0-pendingDocs

--- index.js
+++ index.js
@@ -1,34 +1,71 @@
 // Brief: Access the database at https://github.com/SomajitDey/mime-db-cdn/tree/database
 // Note: Must be runtime agnostic, i.e. should work in browsers, Node.js and server-less V8.
 
-// Params: <String>, MIME-type specification of format <type>/<subtype>[;<parameter>=<value>]
-// Ref: https://developer.mozilla.org/en-US/docs/Web/HTTP/Guides/MIME_types#structure_of_a_mime_type
-// Returns: <Array>
-// On error, rejects with message: 'Not Found'
-export async function mimeToExtensions (mimeTypeSpec) {
-  const [ mimeType ] = mimeTypeSpec.split(';'); // Removes trailing ;<parameter>=<value> if any
-  const dbUrl = `https://cdn.jsdelivr.net/npm/mime-db-cdn@latest/mime-types/${mimeType}/data.json`;
-  const { extensions } = await fetch(dbUrl)
-  .then((response) => response.json())
-  .catch((err) => {
-    throw new Error('Not Found');
-  });
-  return extensions ?? [];
-}
+export default class {
+  cache; // Holds the LRU-Cache if any
+  #cacheInit = {}; // Holds LRU-Cache init object
+  fetch = globalThis.fetch;
 
-// Params: <String>, path | filename with extension | extension with or without the leading dot
-// Returns: <Array>
-// On error, rejects with message: 'Not Found'
-export async function extensionToMimes (path) {
-  const extension = path
-    .split('/').pop() // Removed directory string
-    .split('.').pop(); // Removed filename string
-  // The above works even if `path` is just the extension, e.g. path = 'pdf' or path = '.pdf'
-  const dbUrl = `https://cdn.jsdelivr.net/npm/mime-db-cdn@latest/extensions/${extension}/data.json`;
-  const { mimeTypes } = await fetch(dbUrl)
-  .then((response) => response.json())
-  .catch((err) => {
-    throw new Error('Not Found');
-  });
-  return mimeTypes ?? [];
+  constructor ({ fetch, cacheMaxEntries } = {}) {
+    if (fetch) this.fetch = fetch;
+    if (cacheMaxEntries) this.#cacheInit.max = cacheMaxEntries;
+  }
+
+  async #getCache () {
+    if (this.cache === undefined && Object.keys(this.#cacheInit).length !== 0) {
+      const { LRUCache } = await import('lru-cache');
+      this.cache = new LRUCache(this.#cacheInit);
+    }
+    return this.cache;
+  }
+
+  // Params: <String>, MIME-type specification of format <type>/<subtype>[;<parameter>=<value>]
+  // Ref: https://developer.mozilla.org/en-US/docs/Web/HTTP/Guides/MIME_types#structure_of_a_mime_type
+  // Returns: <Array>
+  // On error, rejects with message: 'Not Found'
+  async mimeToExtensions (mimeTypeSpec) {
+    const [mimeType] = mimeTypeSpec.split(';'); // Removes trailing ;<parameter>=<value> if any
+    const cache = await this.#getCache();
+    let extensions = cache?.get(mimeType);
+    if (extensions === undefined) {
+      const dbUrl = `https://cdn.jsdelivr.net/npm/mime-db-cdn@latest/mime-types/${mimeType}/data.json`;
+      (
+        { extensions } = await this.fetch(dbUrl)
+          .then((response) => {
+            if (response.status === 404) throw new Error('Not Found');
+            return response.json();
+          })
+      );
+      cache?.set(mimeType, extensions);
+    }
+    return extensions ?? [];
+  }
+
+  getExtensions = this.mimeToExtensions; // Alias
+
+  // Params: <String>, path | filename with extension | extension with or without the leading dot
+  // Returns: <Array>
+  // On error, rejects with message: 'Not Found'
+  async extensionToMimes (path) {
+    const extension = path
+      .split('/').pop() // Removed directory string
+      .split('.').pop(); // Removed filename string
+    // The above works even if `path` is just the extension, e.g. path = 'pdf' or path = '.pdf'
+    const cache = await this.#getCache();
+    let mimeTypes = cache?.get(extension);
+    if (mimeTypes === undefined) {
+      const dbUrl = `https://cdn.jsdelivr.net/npm/mime-db-cdn@latest/extensions/${extension}/data.json`;
+      (
+        { mimeTypes } = await this.fetch(dbUrl)
+          .then((response) => {
+            if (response.status === 404) throw new Error('Not Found');
+            return response.json();
+          })
+      );
+      cache?.set(extension, mimeTypes);
+    }
+    return mimeTypes ?? [];
+  }
+
+  getTypes = this.extensionToMimes; // Alias
 }
--- test.js
+++ test.js
@@ -1,9 +1,12 @@
-import { mimeToExtensions, extensionToMimes } from './index.js';
+import DB from './index.js';
 import { describe, test } from 'node:test';
 import assert from 'node:assert';
 
+const dbWithCache = new DB({ cacheMaxEntries: 2 }); // DONOT change the 2. Used in eviction test below.
+const dbWithoutCache = new DB();
+
 describe('Testing index.js', () => {
-  test('mimeToExtensions()', async () => {
+  test('mimeToExtensions() or getExtensions()', async () => {
     const extension = 'js';
     const mimeTypeSpecs = [
       'application/javascript; charset=utf-8',
@@ -12,7 +15,13 @@ describe('Testing index.js', () => {
 
     for (const mimeTypeSpec of mimeTypeSpecs) {
       assert.equal(
-        await mimeToExtensions(mimeTypeSpec)
+        await dbWithCache.getExtensions(mimeTypeSpec)
+          .then((extensions) => extensions.includes(extension)),
+        true
+      );
+
+      assert.equal(
+        await dbWithoutCache.getExtensions(mimeTypeSpec)
           .then((extensions) => extensions.includes(extension)),
         true
       );
@@ -22,12 +31,17 @@ describe('Testing index.js', () => {
   test('Error for mimeToExtensions()', async () => {
     const fakeMimeType = 'type/subtype';
     assert.rejects(
-      mimeToExtensions(fakeMimeType),
+      dbWithCache.mimeToExtensions(fakeMimeType),
+      { message: 'Not Found' }
+    );
+
+    assert.rejects(
+      dbWithoutCache.mimeToExtensions(fakeMimeType),
       { message: 'Not Found' }
     );
   });
 
-  test('extensionToMimes()', async () => {
+  test('extensionToMimes() or getTypes()', async () => {
     const mimeType = 'application/javascript';
     const args = [
       'dir/subdir/path.js',
@@ -38,7 +52,13 @@ describe('Testing index.js', () => {
     ];
     for (const arg of args) {
       assert.equal(
-        await extensionToMimes(arg)
+        await dbWithCache.getTypes(arg)
+          .then((mimeTypes) => mimeTypes.includes(mimeType)),
+        true
+      );
+
+      assert.equal(
+        await dbWithoutCache.getTypes(arg)
           .then((mimeTypes) => mimeTypes.includes(mimeType)),
         true
       );
@@ -48,8 +68,43 @@ describe('Testing index.js', () => {
   test('Error for extensionToMimes()', async () => {
     const fakeExtension = 'extension';
     assert.rejects(
-      extensionToMimes(fakeExtension),
+      dbWithCache.extensionToMimes(fakeExtension),
+      { message: 'Not Found' }
+    );
+
+    assert.rejects(
+      dbWithoutCache.extensionToMimes(fakeExtension),
       { message: 'Not Found' }
     );
   });
+
+  test('LRU Cache eviction', async () => {
+    // Entry #1
+    await dbWithCache.getTypes('exe');
+    const cache = dbWithCache.cache; // Only stores upto 2 entries
+    assert.ok(cache.has('exe'), new Error('Cache is not getting set'));
+    // Entry #2
+    await dbWithCache.getTypes('mp4');
+    assert.ok(cache.has('mp4'), new Error('Cache is not getting set'));
+    // Test repetition, should not be entry #3
+    await dbWithCache.getTypes('mp4');
+    assert.ok(cache.has('exe'), new Error('Cache is not getting set'));
+    assert.ok(cache.has('mp4'), new Error('Cache is not getting set'));
+    // Testing eviction, entry #3. 'exe' should be evicted as its LRU
+    await dbWithCache.getExtensions('application/javascript');
+    assert.equal(cache.has('exe'), false, 'Cache eviction failed');
+    assert.ok(cache.has('mp4'), new Error('Cache is not getting set'));
+    assert.ok(cache.has('application/javascript'), new Error('Cache is not getting set'));
+  });
+  
+  test('Custom fetch', async () => {
+      const extensions = ['test', 'string'];
+      const dbWithCustomFetch = new DB({
+        fetch: async () => new Response(JSON.stringify({ extensions })) 
+      });
+      assert.deepStrictEqual(
+        await dbWithCustomFetch.getExtensions('Test/MIME; any=random'),
+        extensions
+      );    
+  })
 });
